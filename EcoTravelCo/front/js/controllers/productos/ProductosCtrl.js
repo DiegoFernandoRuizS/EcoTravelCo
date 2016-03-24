42ba4256666a0e3f1b0a91737e63b029d811e538
Diego F. Ruiz S <yego23@gmail.com>
2016-03-24 09:19:46 -0500

    Manejo imagenes 1

--- EcoTravelCo/front/js/controllers/productos/ProductosCtrl.js
+++ EcoTravelCo/front/js/controllers/productos/ProductosCtrl.js
@@ -1,95 +1,95 @@
 'use strict';
 
-
 angular.module('materialAdmin')
-  .controller('ProductosCtrl', function ($scope, $rootScope, $http, $location) {
-	  
-      	$scope.datos = [];
-      	$scope.data = {};	    
+        .controller('ProductosCtrl', function ($scope, $rootScope, $http, $location) {
 
- $http.get("http://localhost:8181/producto/").success(function(res){
-            console.log("Consultando productos...");
-                $scope.datos=res
+            $scope.datos = [];
+            $scope.data = {};
 
-        	    console.log("La respuesta en consultar: "+res);
-        	    }).error(function(res){
-        	        console.log("Doesn't work");
-        	        console.log("Que trae esto: "+res);
-        	    });
+            $http.get("http://localhost:8181/producto/").success(function (res) {
+                console.log("Consultando productos...");
+                $scope.datos = res
 
-	     $scope.insertarProducto = function () {
-	     console.log("Insertar producto en el controlador");
+                console.log("La respuesta en consultar: " + res);
+            }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+            });
 
-	     $http.post("http://localhost:8181/producto/",$scope.producto,{})
-        	    .success(function(res){
-              	$scope.insertarProducto = {};
+            $scope.insertarProducto = function () {
+                console.log("Insertar producto en el controlador " +$scope.producto);
+                console.log("Insertar producto en el controlador " +$scope.producto.imagen);
 
-              		    console.log("La respuesta del backend "+res);
-        	    }).error(function(res){
-        	        console.log("Doesn't work para insertar producto");
-        	        console.log("El error para insertar producto: "+res);
-        	    });
-        		};
+                $http.post("http://localhost:8181/producto/", $scope.producto, {})
+                        .success(function (res) {
+                            $scope.insertarProducto = {};
 
-        $scope.borrarProducto = function (id) {
+                            console.log("La respuesta del backend " + res);
+                        }).error(function (res) {
+                    console.log("Doesn't work para insertar producto");
+                    console.log("El error para insertar producto: " + res);
+                });
+            };
 
-             console.log("Borrar producto en el controlador "+id);
+            $scope.borrarProducto = function (id) {
 
-            $http.delete("http://localhost:8181/producto/"+id,$scope.productoDelete,{})
-                                    .success(function(res){
-                                    $scope.borrarProducto = {};
+                console.log("Borrar producto en el controlador " + id);
 
-                                            console.log("La respuesta del backend "+res);
-                                    }).error(function(res){
-                                        console.log("Doesn't work para Borrar producto");
-                                        console.log("El error para borar producto: "+res);
-                                    });
-                                    };
+                $http.delete("http://localhost:8181/producto/" + id, $scope.productoDelete, {})
+                        .success(function (res) {
+                            $scope.borrarProducto = {};
 
-  });
-
-
-angular.module('materialAdmin')
-	.controller('ProductosHomeCtrl', function ($scope, $rootScope, $http, $location) {
-
-		$scope.datos = [];
-		$scope.data = {};
-
-		console.log("Consultando productos...");
-		$http.get("http://localhost:8181/producto_home/")
-
-			.success(function(res){
-				$scope.datos=res
-				console.log(res);
-
-			}).error(function(res){
-			console.log("Doesn't work");
-			console.log("Que trae esto: "+res);
-
-		});
-
-
-
-	});
-
-angular.module('materialAdmin')
-    .controller('ProductosDetalle', function ($scope, $rootScope, $http, $location) {
-
-        $scope.datos = [];
-        $scope.data = {};
-
-        console.log("Consultando productos 22222...");
-        $http.get("http://localhost:8181/producto_detalle/1")
-
-            .success(function(res){
-                $scope.datos=res
-                console.log(res);
-
-            }).error(function(res){
-            console.log("Doesn't work");
-           console.log("Que trae esto: "+res);
+                            console.log("La respuesta del backend " + res);
+                        }).error(function (res) {
+                    console.log("Doesn't work para Borrar producto");
+                    console.log("El error para borar producto: " + res);
+                });
+            };
 
         });
-    });
+
+
+angular.module('materialAdmin')
+        .controller('ProductosHomeCtrl', function ($scope, $rootScope, $http, $location) {
+
+            $scope.datos = [];
+            $scope.data = {};
+
+            console.log("Consultando productos...");
+            $http.get("http://localhost:8181/producto_home/")
+
+                    .success(function (res) {
+                        $scope.datos = res
+                        console.log(res);
+
+                    }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+
+            });
+
+
+
+        });
+
+angular.module('materialAdmin')
+        .controller('ProductosDetalle', function ($scope, $rootScope, $http, $location) {
+
+            $scope.datos = [];
+            $scope.data = {};
+
+            console.log("Consultando productos 22222...");
+            $http.get("http://localhost:8181/producto_detalle/1")
+
+                    .success(function (res) {
+                        $scope.datos = res
+                        console.log(res);
+
+                    }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+
+            });
+        });
 
 
