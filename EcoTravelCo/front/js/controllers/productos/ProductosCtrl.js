319ec3529e9f673a0c5fb3fa65f99069f0ae643a
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-26 16:33:54 -0500

    commit de galeria y mejora de detalle de producto
    commit de galeria y mejora de detalle de producto

--- EcoTravelCo/front/js/controllers/productos/ProductosCtrl.js
+++ EcoTravelCo/front/js/controllers/productos/ProductosCtrl.js
@@ -1,51 +1,51 @@
 'use strict';
 
 angular.module('materialAdmin')
-        .controller('ProductosCtrl', function ($scope, $rootScope, $http, $location) {
+    .controller('ProductosCtrl', function ($scope, $rootScope, $http, $location) {
 
-            $scope.datos = [];
-            $scope.data = {};
-            $rootScope.load = 0;
-            $scope.listaValores = [];
-
-
-            $scope.queryAllProduct = function () {
-                //var id = pathArray[pathArray.length-2];
-                $http({method: 'GET', url: 'http://localhost:8181/producto_home/'})
-                        .success(function (res) {
-                            $scope.datos = res
-                            $rootScope.load = 1;
-                        }).error(function (res) {
-                    console.log("Doesn't work");
-                    console.log("Que trae esto: " + res);
-                })
-            };
+        $scope.datos = [];
+        $scope.data = {};
+        $rootScope.load = 0;
+        $scope.listaValores = [];
 
 
-            $scope.queryProducts = function () {
-                $rootScope.busquedaProd = $scope.producto.search;
-            };
+        $scope.queryAllProduct = function () {
+            //var id = pathArray[pathArray.length-2];
+            $http({method: 'GET', url: 'http://localhost:8181/producto_home/'})
+                .success(function (res) {
+                    $scope.datos = res
+                    $rootScope.load = 1;
+                }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+            })
+        };
 
-            if ($rootScope.load == 0) {
-                $scope.queryAllProduct();
-            }
 
-            $scope.detailProd = function (id) {
-                $rootScope.prodId = id;
-            };
+        $scope.queryProducts = function () {
+            $rootScope.busquedaProd = $scope.producto.search;
+        };
+
+        if ($rootScope.load == 0) {
+            $scope.queryAllProduct();
+        }
+
+        $scope.detailProd = function (id) {
+            $rootScope.prodId = id;
+        };
 
 
-        });
+    });
 
 angular.module('materialAdmin')
-        .controller('ProductosCtrlProveedor', function ($scope, $rootScope, $http, $location) {
+    .controller('ProductosCtrlProveedor', function ($scope, $rootScope, $http, $location) {
 
-            $scope.datos = [];
-            $scope.listaValores = [];
-            $scope.productoEditar=[];
+        $scope.datos = [];
+        $scope.listaValores = [];
+        $scope.productoEditar=[];
 
-           //para cargar el combox box de paises
-           $scope.combox=function(){
+        //para cargar el combox box de paises
+        $scope.combox=function(){
             $http.get("http://localhost:8181/datos", {
                 withCredentials: true,
                 headers: {token: sessionStorage.token}
@@ -55,106 +55,125 @@ angular.module('materialAdmin')
                 console.log("Doesn't work");
                 console.log("Que trae esto paises " + res);
             });
-            };
+        };
 
-            $scope.consultarProductos = function () {
-                $http.get("http://localhost:8181/producto/", {
-                    withCredentials: true,
-                    headers: {token: sessionStorage.token}
-                }).success(function (res) {
-                    console.log("Consultando productos...");
-                    $scope.datos = res
+        $scope.consultarProductos = function () {
+            $http.get("http://localhost:8181/producto/", {
+                withCredentials: true,
+                headers: {token: sessionStorage.token}
+            }).success(function (res) {
+                console.log("Consultando productos...");
+                $scope.datos = res
 
-                    //  console.log("La respuesta en consultar: " + res);
+                //  console.log("La respuesta en consultar: " + res);
+            }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+            });
+        };
+
+        $scope.insertarProducto = function () {
+            var i = document.getElementById('imagen').files[0];
+            var i2 = document.getElementById('imagen2').files[0];
+            var i3 = document.getElementById('imagen3').files[0];
+
+            var imagenBytes = i.result;
+            var imagenBytes2 = i2.result;
+            var imagenBytes3 = i3.result;
+            $scope.producto.imagen = imagenBytes;
+            $scope.producto.imagen2 = imagenBytes2;
+            $scope.producto.imagen3 = imagenBytes3;
+
+
+            $http.post("http://localhost:8181/producto/", $scope.producto, {withCredentials: true, headers: {token: sessionStorage.token}})
+                .success(function (res) {
+                    $scope.insertarProducto = {};
+                    console.log("La respuesta del backend " + res);
+                    $scope.consultarProductos();
+
                 }).error(function (res) {
-                    console.log("Doesn't work");
-                    console.log("Que trae esto: " + res);
-                });
-            };
+                console.log("Doesn't work para insertar producto");
+                console.log("El error para insertar producto: " + res);
+            });
+        };
 
-            $scope.insertarProducto = function () {
-                var i = document.getElementById('imagen').files[0];
-                var i2 = document.getElementById('imagen2').files[0];
-                var i3 = document.getElementById('imagen3').files[0];
+        $scope.borrarProducto = function (id) {
+            console.log("Borrar producto en el controlador " + id);
+            $http.delete("http://localhost:8181/producto/" + id, $scope.productoDelete, {withCredentials: true, headers: {token: sessionStorage.token}})
+                .success(function (res) {
+                    $scope.borrarProducto = {};
+                    $scope.consultarProductos();
+                    //console.log("La respuesta del backend " + res);
+                }).error(function (res) {
+                console.log("Doesn't work para Borrar producto");
+                console.log("El error para borar producto: " + res);
+            });
+        };
 
-                var imagenBytes = i.result;
-                var imagenBytes2 = i2.result;
-                var imagenBytes3 = i3.result;
-                $scope.producto.imagen = imagenBytes;
-                $scope.producto.imagen2 = imagenBytes2;
-                $scope.producto.imagen3 = imagenBytes3;
+        $scope.listarProducto = function (id) {
+            console.log("Listar producto en el controlador " + id);
+            $http.get("http://localhost:8181/producto/" + id,{withCredentials: true, headers: {token: sessionStorage.token}})
+                .success(function (res) {
+                    $scope.productoEditar = res;
+                    console.log($scope.productoEditar);
+                }).error(function (res) {
+                console.log("Doesn't work para listar producto");
+                console.log("El error para borar producto: " + res);
+            });
+        };
+
+        $scope.consultarProductos();
+        $scope.combox();
+    });
 
 
-                $http.post("http://localhost:8181/producto/", $scope.producto, {withCredentials: true, headers: {token: sessionStorage.token}})
-                        .success(function (res) {
-                            $scope.insertarProducto = {};
-                            console.log("La respuesta del backend " + res);
-                            $scope.consultarProductos();
-
-                        }).error(function (res) {
-                    console.log("Doesn't work para insertar producto");
-                    console.log("El error para insertar producto: " + res);
-                });
-            };
-
-            $scope.borrarProducto = function (id) {
-                console.log("Borrar producto en el controlador " + id);
-                $http.delete("http://localhost:8181/producto/" + id, $scope.productoDelete, {withCredentials: true, headers: {token: sessionStorage.token}})
-                        .success(function (res) {
-                            $scope.borrarProducto = {};
-                            $scope.consultarProductos();
-                            //console.log("La respuesta del backend " + res);
-                        }).error(function (res) {
-                    console.log("Doesn't work para Borrar producto");
-                    console.log("El error para borar producto: " + res);
-                });
-            };
-
-            $scope.listarProducto = function (id) {
-                console.log("Listar producto en el controlador " + id);
-                $http.get("http://localhost:8181/producto/" + id,{withCredentials: true, headers: {token: sessionStorage.token}})
-                        .success(function (res) {
-                            $scope.productoEditar = res;
-                            console.log($scope.productoEditar);
-                        }).error(function (res) {
-                    console.log("Doesn't work para listar producto");
-                    console.log("El error para borar producto: " + res);
-                });
-            };
-
-            $scope.consultarProductos();
-            $scope.combox();
-        });
 
 angular.module('materialAdmin')
-        .controller('ProductosDetalle', function ($scope, $rootScope, $http, $location) {
+    .controller('ProductosDetalle', function ($scope, $rootScope, $http, $location) {
 
-            $scope.datos = [];
-            $scope.data = {};
+        $scope.datos = [];
+        $scope.data = {};
 
-            $scope.comentario = [];
-            $scope.comentarios = {};
-
-            $scope.detailProd = function () {
-
-                var id = $rootScope.prodId;
-
-                console.log("Entro?" + id);
-                $http({method: 'GET', url: 'http://localhost:8181/producto_detalle/' + id})
-                        .success(function (res) {
-                            $scope.datos = res
-                            console.log(res);
-
-                        }).error(function (res) {
-                    console.log("Doesn't work");
-                    console.log("Que trae esto: " + res);
-                })
-            };
+        $scope.myInterval = 0;
+        $scope.galeria = [];
 
 
-            $scope.detailProd();
+        $scope.comentario = [];
+        $scope.comentarios = {};
 
-        });
+        $scope.detailProd = function () {
+
+            var id=  $rootScope.prodId;
+
+            console.log("Entro?"+id);
+            $http({method: 'GET', url: 'http://localhost:8181/producto_detalle/' + id})
+                .success(function(res){
+                    $scope.datos=res
+                    console.log(res);
+
+                }).error(function(res){
+                console.log("Doesn't work");
+                console.log("Que trae esto: "+res);
+            })
+
+
+            $http({method: 'GET', url: 'http://localhost:8181/galeria/' + id})
+                .success(function(res){
+                    $scope.galeria=res
+                    console.log(res);
+
+                }).error(function(res){
+                console.log("Doesn't work");
+                console.log("Que trae esto: "+res);
+            })
+
+        };
+
+
+        $scope.detailProd();
+
+    });
+
 
 
 
@@ -162,22 +181,22 @@ angular.module('materialAdmin')
 
     .controller('ProductosBusqueda', function ($scope, $rootScope, $http, $location, $filter, $sce, ngTableParams, tableService) {
 
-            $scope.datos = [];
-            $scope.data = {};
+        $scope.datos = [];
+        $scope.data = {};
 
-            $scope.comentario = [];
-            $scope.comentarios = {};
+        $scope.comentario = [];
+        $scope.comentarios = {};
 
-            $scope.queryProducts = function () {
+        $scope.queryProducts = function () {
 
-                var criterios = $rootScope.busquedaProd;
+            var criterios = $rootScope.busquedaProd;
 
             var criterios=  $rootScope.busquedaProd;
             $http({method: 'GET', url: 'http://localhost:8181/producto_busqueda/' + criterios})
                 .success(function(res){
                     $scope.datos=res;
 
-                   $scope.tableBasic = new ngTableParams(
+                    $scope.tableBasic = new ngTableParams(
                         {page: 1, count: 5},
                         {
                             total: res.length,
@@ -190,11 +209,11 @@ angular.module('materialAdmin')
                     );
 
                     console.log(res);
-                        }).error(function (res) {
-                    console.log("Doesn't work");
-                    console.log("Que trae esto: " + res);
-                })
-            };
+                }).error(function (res) {
+                console.log("Doesn't work");
+                console.log("Que trae esto: " + res);
+            })
+        };
 
 
         $scope.detailProd = function (id) {
@@ -203,4 +222,4 @@ angular.module('materialAdmin')
 
         $scope.queryProducts();
 
-        });
+    });
