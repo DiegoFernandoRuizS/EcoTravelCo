d18730679c40a3063ec9cc7fb2b1b233119cd5ad
Diego F. Ruiz S <yego23@gmail.com>
2016-04-24 17:44:52 -0500

    update auth

--- EcoTravelCo/front/js/fbauth.js
+++ EcoTravelCo/front/js/fbauth.js
@@ -1,17 +1,7 @@
 $(function() {
 	var app_id = '985703204846099';
 	var scopes = 'email, user_friends, user_online_presence';
-
-	var btn_login = '<a href="" data-ng-click="autenticarfb();" id="loginFB"><img src="img/social/login-with-facebook.png" alt="Sign in with Facebook"></a>';
-
-	var div_session = "<div id='facebook-session'>"+
-					  "<strong></strong>"+
-					  "<img>"+
-					  "<a href='#' id='logout' class='btn btn-danger'>Cerrar sesión</a>"+
-					  "</div>";
-
     console.log("entra");
-
 	window.fbAsyncInit = function() {
 
 	  	FB.init({
@@ -34,7 +24,7 @@ $(function() {
     	if (response.status === 'connected') {
       		getFacebookData();
     	} else {
-     		callback(false);
+     	//	callback(false);
     	}
   	}
 
@@ -45,14 +35,25 @@ $(function() {
   	}
 
   	var getFacebookData =  function() {
-            console.log("Trae los datos");
-  		FB.api('/me', function(response) {
-	  		$('#loginFB').after(div_session);
-	  		$('#loginFB').remove();
-	  		$('#facebook-session strong').text("Bienvenido: "+response.name);
-	  		$('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
+        console.log("Trae los datos");
+        FB.api('/me?fields=id,name,email,permissions', function(response){
+        console.log(response);
+  		if(response.name!=null){
+  		            	   //sessionStorage.token = res.token;
+                            sessionStorage.setItem ("nombreusuario",response.name);
+                            sessionStorage.setItem ("correousuario",response.email) ;
+                            sessionStorage.setItem ("tipo","CLIENTE");
+                            sessionStorage.setItem ("foto",'http://graph.facebook.com/'+response.id+'/picture?type=large');
+                            // console.log(sessionStorage.getItem("nombreusuario"));
+                            console.log(response);
+
+        window.location.href = 'http://localhost:9291/#/home';
+  		}else{
+
+  		}
+
 	  	});
-             //window.location.href = 'http://localhost:9291/#/home';
+
   	}
 
   	var facebookLogin = function() {
@@ -73,8 +74,8 @@ $(function() {
   			if (data.status === 'connected') {
                             console.log("Cierra sesión");
 				FB.logout(function(response) {
-					$('#facebook-session').before(btn_login);
-					$('#facebook-session').remove();
+				window.location.href = 'http://localhost:9291/login.html#/home';
+
 				})
 			}
   		})
@@ -91,11 +92,7 @@ $(function() {
 
   	$(document).on('click', '#logout', function(e) {
   		e.preventDefault();
-
-  		if (confirm("¿Está seguro?"))
-  			facebookLogout();
-  		else 
-  			return false;
+	    facebookLogout();
   	})
 
 })
