a5c6f57837862b65c5131f43fd7fc265d21766b9
seduardojs@gmail.com <seduardojs@gmail.com>
2016-05-15 10:52:44 +0100

    Lista de Caracteristicas Seleccionadas

--- EcoTravelCo/derivador/src/co/ecofactory/derivador/Main.java
+++ EcoTravelCo/derivador/src/co/ecofactory/derivador/Main.java
@@ -9,33 +9,41 @@ import java.nio.file.StandardOpenOption;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	    String rutaPOM = args[0];
+        String rutaPOM = args[0];
         String rutaEjecutable = args[1];
-        String activos = args[2];
+        String rutaConfig = args[2];
 
         String pom = new String(Files.readAllBytes(Paths.get(rutaPOM)));
-
-
-        String[] modulos = activos.split(";");
+        String[] modulos = Files.readAllLines(Paths.get(rutaConfig)).toArray(new String[]{});
 
         String modulosPOM = "";
         String modulosEjecutable = "";
 
-        for ( int i = 0 ; i < modulos.length ; i++){
+        for (int i = 0; i < modulos.length; i++) {
             String modulo = modulos[i];
-            if ( modulo.equals("MESSAGE") ){
-                modulosPOM += "<dependency>\n" +
-                        "            <groupId>co.ecofactory</groupId>\n" +
-                        "            <artifactId>mod-message</artifactId>\n" +
-                        "            <version>3.2.1</version>\n" +
-                        "        </dependency>";
 
-                modulosEjecutable += " -Dmensajes=ACTIVO ";
+            if (!modulo.startsWith("CORE_")) {
+
+                if (modulo.equals("EnvioMensajes")) {
+                    modulosPOM += "<dependency>\n" +
+                            "            <groupId>co.ecofactory</groupId>\n" +
+                            "            <artifactId>mod-message</artifactId>\n" +
+                            "            <version>3.2.1</version>\n" +
+                            "        </dependency>";
+                }
+                if (modulo.equals("CalificarServicios")) {
+                    modulosPOM += "<dependency>\n" +
+                            "            <groupId>co.ecofactory</groupId>\n" +
+                            "            <artifactId>mod-calificacion</artifactId>\n" +
+                            "            <version>3.2.1</version>\n" +
+                            "        </dependency>";
+                }
+                modulosEjecutable += " -D" + modulo + "=ACTIVO ";
             }
         }
 
-        if (!modulosPOM.equals("")){
-            pom=pom.replaceAll("<!--modulos-->",modulosPOM);
+        if (!modulosPOM.equals("")) {
+            pom = pom.replaceAll("<!--modulos-->", modulosPOM);
         }
 
 
